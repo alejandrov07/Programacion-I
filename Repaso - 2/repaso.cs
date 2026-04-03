acde6dc6585ff6cf88c717561d450e6151f23503
Alejandro Velázquez <avelazquez1@est.unibe.edu.do>
2026-04-03 12:50:45 -0400

    Add Bus class with properties and methods

--- Repaso - 2/repaso.cs
+++ Repaso - 2/repaso.cs
@@ -1,1 +1,44 @@
+using System;
 
+class Bus
+{
+    public string Nombre;
+    public int Asientos;
+    public int Precio;
+    public int Pasajeros;
+
+    public Bus(string nombre, int asientos, int precio, int pasajeros)
+    {
+        Nombre = nombre;
+        Asientos = asientos;
+        Precio = precio;
+        Pasajeros = pasajeros;
+    }
+
+    public int Ventas()
+    {
+        return Precio * Pasajeros;
+    }
+
+    public int Disponibles()
+    {
+        return Asientos - Pasajeros;
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine("Autobus " + Nombre + ", " + Pasajeros + " Pasajeros, Ventas " + Ventas() + ", quedan " + Disponibles() + " asientos disponibles");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bus bus1 = new Bus("Platinum", 22, 1000, 5);
+        Bus bus2 = new Bus("Gold", 9, 1333, 3);
+        
+        bus1.Mostrar();
+        bus2.Mostrar();
+    }
+}
